993547117dd74dca0d395479322010f4c9875642
debian <debian@debian.org>
2024-11-14 17:53:22 +0100

    Exported OpenAI features to another file

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import streamlit as st
-from openai import OpenAI
-import base64
-import instructor
-from pydantic import BaseModel
-from dotenv import dotenv_values
 from database import return_reqest , connect_to_db, disconnect
+from login_screen import login_screen
+from ai_func import fill_meal, Meal
 
 class CalorieAndMacroToday:
     calories: int
@@ -13,86 +10,36 @@ class CalorieAndMacroToday:
     fats: int
     fiber: int
 
-class Meal(BaseModel):
-    name: str
-    calories: int
-    protein: int
-    carbs: int
-    fats: int
-    fiber: int
 
-def fill_meal(image):
-    meal = instructor_openai_client.chat.completions.create(
-        model="gpt-4o-mini",
-        response_model=Meal,
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": "Wypełnij makroskładniki i kalorie posiłku oraz podaj jego nazwę.", 
-                    },
-                    {
-                        "type": "image_url",
-                        "image_url": {
-                            "url": image,
-                            "detail": "high"
-                        },
-                    },
-                ],
-            },
-        ],
-    )
-    return meal
+if 'usr_id' not in st.session_state:
+        st.session_state.usr_id = login_screen()
+        
 
-def prepare_image_for_open_ai(image_path):
-    with open(image_path, "rb") as f:
-        image_data = base64.b64encode(f.read()).decode('utf-8')
-    return f"data:image/png;base64,{image_data}"
-
-today = CalorieAndMacroToday()
-meals = []
-
-# Connecting to OpenAI
-env = dotenv_values(".env")
-openai_client = OpenAI(api_key=env["OPENAI_API_KEY"])
-instructor_openai_client = instructor.from_openai(openai_client)
-
-st.title("Camera or File Upload Example")
-
+if st.session_state.usr_id is not None:
 # Option to either upload a photo or take a picture from the camera
-option = st.selectbox("Choose an option", ("Upload a photo", "Take a picture from camera"))
+    option = st.selectbox("Choose an option", ("Upload a photo", "Take a picture from camera"))
 
-image = None
-if option == "Upload a photo":
-    image = st.file_uploader("Upload a photo", type=["png", "jpg", "jpeg"])
-elif option == "Take a picture from camera":
-    image = st.camera_input("Take a picture")
+    image = None
+    if option == "Upload a photo":
+        image = st.file_uploader("Upload a photo", type=["png", "jpg", "jpeg"])
+    elif option == "Take a picture from camera":
+        image = st.camera_input("Take a picture")
 
-connection = connect_to_db()
-test = return_reqest(connection, "SELECT version();")
-st.write(test)
-disconnect(connection)
 
-# If an image is provided, display it and prepare it for OpenAI
-if image is not None:
-    with open("captured_image.png", "wb") as f:
-        f.write(image.getbuffer())
 
-    prepared_image = prepare_image_for_open_ai("captured_image.png")
+    # If an image is provided, display it and prepare it for OpenAI
+    if image is not None:
+        with open("captured_image.png", "wb") as f:
+            f.write(image.getbuffer())
 
-    st.image(image, caption="Captured Image", use_column_width=True)
-
-    st.text(prepared_image)
-    
-    meal = fill_meal(prepared_image)
-    
-    # Print out meal macros
-    st.write(meal.name)
-    st.write("Kalorie: " + str(meal.calories))
-    st.write("Białko: " + str(meal.protein))
-    st.write("Węglowodany: " + str(meal.carbs))
-    st.write("Tłuszcze: " + str(meal.fats))
-    st.write("Błonnik: " + str(meal.fiber))
+        
+        meal = fill_meal("captured_image.png")
+        
+        # Print out meal macros
+        st.write(meal.name)
+        st.write("Kalorie: " + str(meal.calories))
+        st.write("Białko: " + str(meal.protein))
+        st.write("Węglowodany: " + str(meal.carbs))
+        st.write("Tłuszcze: " + str(meal.fats))
+        st.write("Błonnik: " + str(meal.fiber))
     
